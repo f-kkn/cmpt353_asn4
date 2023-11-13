7c2984845837e201a020a169d61e72482e9300e8
flyte <flyte.1ne@gmail.com>
2023-11-13 01:35:40 -0600

    Did all of the functions in server.js, Delete db.js

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,18 @@
 const mysql = require('mysql');
-const connParam = {
+const connection = mysql.createConnection({
     host: "mysql1",
     user: "root",
     password: "secret"
-};
-exports.initializeConnection = async (dbName, tableName) => {
-    var connection = mysql.createConnection(connParam);
+});
+var serverConnected = false;
+
+exports.initializeAll = (dbName, tableName) => {
+    connection.connect()
+}
+
+
+
+exports.initializeAll = async (dbName, tableName) => {
     connection.connect((err) => {
         if(err){ throw err;}
     });
@@ -17,12 +24,73 @@ exports.initializeConnection = async (dbName, tableName) => {
         }
     });
     var getDatabase = `USE ${dbName}`;
+    var checkTable = `DROP TABLE IF EXISTS ${tableName}`;
+    var addTable = `CREATE TABLE IF NOT EXISTS ${tableName} (
+        id          int unsigned NOT NULL auto_increment,
+        topic       varchar(100) NOT NULL,
+        data        varchar(100) NOT NULL,
+        PRIMARY KEY (id))`;
     connection.query(getDatabase, (err) => {
         if(err){throw err}
         else{
-            console.log(`Working with ${dbName} database`);
-            
+            console.log(`[db]: Working with ${dbName} database.`);
+            connection.query(checkTable, (err) => { //Delete if it exist first
+                if(err){throw err;}
+                console.log(`[db]: Deleting contents of ${tableName}.`);
+            });
+            connection.query(addTable, (err) => { //Create the table
+                if(err){throw err;}
+                console.log(`[db]: Creating table ${tableName}.`);
+            });
         }
     });
-
 }
+
+exports.setTableConents = (dbName, tableName) => {
+    var connection = mysql.createConnection(connParam);
+    connection.connect((err) => {
+        if(err){ throw err;}
+    });
+    var getDatabase = `USE ${dbName}`;
+    var getTable = `SELECT * FROM ${tableName}`;
+    connection.query(getDatabase, (err) => {
+        if(err){throw err;}
+        else{
+            connection.query(getTable, (err, res) => {
+                if(err){throw err}
+                else{
+                    var insertTable = `INSERT INTO ${tableName} (topic, data) VALUES ('Hello', 'World')`;
+                    connection.query(insertTable, (err) => {
+                        if(err){throw err;}
+                        else{
+                            console.log("Insertion Complete");
+                        }
+                    });
+                }
+                
+            });
+        }
+    });
+}
+
+// exports.getTableContents = (dbName, tableName) => {
+//     var connection = mysql.createConnection(connParam);
+//     connection.connect((err) => {
+//         if(err){ throw err;}
+//     });
+//     var getDatabase = `USE ${dbName}`;
+//     var getTable = `SELECT * FROM ${tableName}`;
+//     connection.query(getDatabase, (err) => {
+//         if(err){throw err;}
+//         else{
+//             connection.query(getTable, (err, res) => {
+//                 if(err){throw err;}
+//                 else{
+//                     return JSON.parse(JSON.stringify(res));
+//                 }
+//             });
+//         }
+//     })
+
+
+// }
--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const backend = require("./backend/db");
+const mysql = require('mysql');
 
-const dbName = "webdb";
-const tableName = "posts";
-
+//connections for express
 const port = 3000;
 const host = '0.0.0.0';
 const app = express();
@@ -12,17 +11,65 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-//var tableCreated = 0;
-app.get('/init', (req, res) => {
-    backend.initializeConnection("postDB", "posts")
-    .then((data) => {
-        console.log("COMPLETE");
-        res.send("");
+
+//Connection for mysql
+var dbName = "webdb";
+var tableName = "posts";
+var isConnected = false;
+const connection = mysql.createConnection({
+    host: "mysql1",
+    user: "root",
+    password: "secret"
+})
+
+
+app.get('/init', (request, response) => {
+    if(isConnected){
+        console.log(`[SERVER] : Already established a connection to mysql.`);
+        return;
+    }
+
+    connection.connect((err) => {
+        if(err){throw err;}
+        console.log(`[SERVER] : Initialize connection to mysql.`)
+        isConnected = true;
     })
-    .catch((err) => {
-        console.log(err);
+
+    var queryCreateDatabase = `CREATE DATABASE IF NOT EXISTS ${dbName}`;
+    connection.query(queryCreateDatabase, (err) => {
+        if(err){throw err;}
+        console.log(`[SERVER] : database \'${dbName}\' created.`);
     });
 
+    var getDatabase = `USE ${dbName}`;
+    var checkTable = `DROP TABLE IF EXISTS ${tableName}`;
+    var addTable = `CREATE TABLE IF NOT EXISTS ${tableName} (
+        id          int unsigned NOT NULL auto_increment,
+        topic       varchar(100) NOT NULL,
+        data        varchar(100) NOT NULL,
+        PRIMARY KEY (id))`;
+
+    connection.query(getDatabase, (err) => {
+        if(err){throw err;}
+        console.log(`[SERVER] : Working with ${dbName} database.`);
+        connection.query(checkTable, (err) => {
+            if(err){throw err;}
+            console.log(`[SERVER] : Deleting contents of ${tableName}.`);
+        });
+        connection.query(addTable, (err) => { //Create the table
+            if(err){throw err;}
+            console.log(`[SERVER] : Creating table ${tableName}.`);
+        });
+    }); 
+});
+
+app.get('/get', (req, res) => {
+    var getTable = `SELECT * FROM ${tableName}`;
+    connection.query(getTable, (err, result) => {
+        if(err){throw err;}
+        if(result.length == 0){console.log("Empty Table")};
+        res.send(result);
+    });
 });
 
 app.listen(port, host);
